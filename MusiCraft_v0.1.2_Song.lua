906861da65b1d8216a2567c7dac700b4a1e2e658
Trebek <Trebek@users.noreply.github.com>
2013-08-16 21:15:25 -0700

    Create MusiCraft_v0.1.2_Song.lua

---
+++ MusiCraft_v0.1.2_Song.lua
@@ -0,0 +1,41 @@
+--[[ INFO ]]--
+
+-- Name: Test Song
+-- Version: N/A
+-- Date: 16/08/2013
+-- Author: Alex Crawford
+-- Notes: N/A
+
+--[[ EXPLANATION ]]--
+
+--[[ 
+
+The first number in each sub-table is the ID of the computer (and instrument) to trigger.
+The second number is the time to rest between notes. Minecraft doesn't seem to be able to handle anything faster than 1/4 (0.25) of a second.
+Example:
+
+{3,0.25,}
+
+3 = ID
+0.25 = Rest time
+
+]]--
+
+--[[ SONG TABLE ]]--
+
+{{3,0.25,},
+{5,0.25,},
+{3,0.25,},
+{5,0.25,},
+{3,0.25,},
+{4,0.25,},
+{3,0.25,},
+{4,0.25,},
+{3,0.25,},
+{5,0.25,},
+{3,0.25,},
+{5,0.25,},
+{3,0.25,},
+{4,0.25,},
+{3,0.25,},
+{4,0.25,},}
