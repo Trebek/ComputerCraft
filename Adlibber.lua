6658a36ddb75303d179e514e7fa1b4437ed5a546
Trebek <Trebek@users.noreply.github.com>
2013-08-18 16:52:15 -0700

    Update Adlibber.lua

--- Adlibber.lua
+++ Adlibber.lua
@@ -1,147 +1,190 @@
 --[[ INFO ]]--
 
 -- Name: Adlibber
--- Version: 0.1.0
--- Dates: 14/08/2013
+-- Version: 0.2.0
+-- Dates: 18/08/2013
 -- Author: Alex Crawford
--- Notes: Just a simple adlib program. Will probably let the user input the words to be used at some point, but for now it just pulls the words from some tables.
+-- Notes: See wiki on GitHub.
 
---[[ NOUN/VERB/ADJECTIVE/WORD TABLES ]]--
+--[[ S01 - WORD / SENTENCE TABLES ]]--
 
-local NounTable1 = { -- List of singular nouns.
-	[1] = "time",
-	[2] = "year",
-	[3] = "people",
-	[4] = "alien",
-	[5] = "butt",
-	[6] = "television",
-	[7] = "computer",
-	[8] = "car",
-	[9] = "gun",
-	[10] = "hair",
-	[11] = "thing",
-	[12] = "child",
-	[13] = "house",
-	[14] = "problem",
-	[15] = "party",
-	[16] = "school",
-	[17] = "eye",
-	[18] = "money",
-	[19] = "water",
-	[20] = "book",
+--[[ S01.1 - Adjectives ]]--
+
+local AdjTable = {
+	[1] = "good",
+	[2] = "new",
+	[3] = "old",
+	[4] = "great",
+	[5] = "high",
+	[6] = "small",
+	[7] = "large",
+	[8] = "smelly",
+	[9] = "rad",
+	[10] = "strange",
 }
 
-local NounTable2 = { -- List of plural nouns.
-	[1] = "times",
-	[2] = "years",
-	[3] = "people",
-	[4] = "aliens",
-	[5] = "butts",
-	[6] = "televisions",
-	[7] = "computers",
-	[8] = "cars",
-	[9] = "guns",
-	[10] = "hairs",
-	[11] = "things",
-	[12] = "children",
-	[13] = "houses",
-	[14] = "problems",
-	[15] = "parties",
-	[16] = "schools",
-	[17] = "eyes",
-	[18] = "money",
-	[19] = "water",
-	[20] = "books",
+--[[ S01.2 - Nouns ]]--
+
+local NounTable = {
+	[1] = {"time", "times"},
+	[2] = {"man", "men",},
+	[3] = {"woman",	"women",},
+	[4] = {"thing", "things",},
+	[5] = {"hand", "hands",},
+	[6] = {"head", "heads",},
+	[7] = {"butt", "butts",},
+	[8] = {"gun", "guns",},
+	[9] = {"face", "faces",},
+	[10] = {"computer", "computers",},
 }
 
-local VerbTable1 = { -- List of plural verbs.
-	[1] = "attack",
-	[2] = "act",
-	[3] = "add",
-	[4] = "breathe",
-	[5] = "blow",
-	[6] = "break",
-	[7] = "create",
-	[8] = "clean",
-	[9] = "drive",
-	[10] = "dance",
-	[11] = "go",
-	[12] = "see",
-	[13] = "know",
-	[14] = "say",
-	[15] = "give",
-	[16] = "take",
-	[17] = "pull",
-	[18] = "feel",
-	[19] = "play",
-	[20] = "stand",
-	[21] = "kill",
+--[[ S01.3 - Verbs ]]--
+
+local VerbTable = { 
+	[1] = {"make","makes","making","made",},
+	[2] = {"become","becomes","becoming","became",},
+	[3] = {"consider","considers","considering","considered",},
+	[4] = {"create","creates","creating","created",},
+	[5] = {"punch","punches","punching","punched",},
+	[6] = {"kick","kicks","kicking","kicked",},
+	[7] = {"jump","jumps","jumping","jumped",},
+	[8] = {"dance","dances","dancing","danced",},
+	[9] = {"throw","throws","throwing","threw",},
+	[10] = {"produce","produces","producing","produced",},
 }
 
-local VerbTable2 = { -- List of transitive verbs.
-	[1] = "attacked",
-	[2] = "acted",
-	[3] = "added",
-	[4] = "breathed",
-	[5] = "blew",
-	[6] = "broke",
-	[7] = "created",
-	[8] = "cleaned",
-	[9] = "drove",
-	[10] = "danced",
-	[11] = "went",
-	[12] = "saw",
-	[13] = "knew",
-	[14] = "said",
-	[15] = "gave",
-	[16] = "took",
-	[17] = "pulled",
-	[18] = "felt",
-	[19] = "played",
-	[20] = "stood",
-	[21] = "killed",
+--[[ S01.4 - Names ]]--
+
+local NameTable = {
+	[1] = "Alex",
+	[2] = "Bob",
+	[3] = "Chris",
+	[4] = "Dan",
+	[5] = "Eric",
+	[6] = "Frank",
+	[7] = "Greg",
+	[8] = "Horace",
+	[9] = "Irving",
+	[10] = "Jack",
 }
 
-local VerbTable3 = { -- List of singular verbs.
-	[1] = "attacks",
-	[2] = "acts",
-	[3] = "adds",
-	[4] = "breathes",
-	[5] = "blows",
-	[6] = "breaks",
-	[7] = "creates",
-	[8] = "cleans",
-	[9] = "drives",
-	[10] = "dances",
-	[11] = "goes",
-	[12] = "sees",
-	[13] = "knows",
-	[14] = "says",
-	[15] = "gives",
-	[16] = "takes",
-	[17] = "pulls",
-	[18] = "feels",
-	[19] = "plays",
-	[20] = "stands",
-	[21] = "kills",
+--[[ S01.5 - Sentence Fragments ]]--
+
+local SentFrags1 = {
+	[1] = "This is a story about a ",
+	[2] = "Once upon a time there lived a ",
+	[3] = "This tale is about a ",
+	[4] = "This story begins with a ",
+	[5] = "A long time ago, in a galaxy far, far away, there lived a ",
 }
 
-local WordTable = { -- Generates the random words, from the noun/verb tables, to be plugged in to the story.
-	[1] = NounTable1[math.random(20)],
-	[2] = NounTable2[math.random(20)],
-	[3] = VerbTable1[math.random(21)],
-	[4] = VerbTable2[math.random(21)],
-	[5] = VerbTable3[math.random(21)],
+local SentFrags2 = {
+	[1] = " that goes by the name of ",
+	[2] = " named ",
+	[3] = " that likes to call themselves ",
+	[4] = " that goes by the name of ",
+	[5] = " that goes by the name of ",
 }
 
---[[ WORD VARIABLES ]]--
+local SentFrags3 = {
+	[1] = "This is a story about a ",
+	[2] = "Once upon a time there lived a ",
+	[3] = "This tale is about a ",
+	[4] = "This story begins with a ",
+	[5] = "A long time ago, in a galaxy far, far away, there lived a ",
+}
 
-local Noun1 = WordTable[1] -- Sets the noun/verb placeholders to the random chosen words, in 'WordTable'.
-local Noun2 = WordTable[2]
-local Verb1 = WordTable[3]
-local Verb2 = WordTable[4]
-local Verb3 = WordTable[5]
+--[[ S02 - WORD / SENTENCE VARIABLES ]]--
 
---[[ THE STORY, WITH INSERTED WORD VARIABLES ]]--
+--[[ S02.1 - Adjective Variables ]]--
 
-print("This is a story about a " .. Noun1 .. ". This tale is not for the faint of heart, as it contains a lot of " .. Noun2 .. " that like to " .. Verb1 .. ". If you don't mind, then read on. This " .. Noun1 .. " " .. Verb2 .. " a man. That man " .. Verb3 .. ", and owns many " .. Noun2 .. ".") -- Prints the story, and inserts the randomized words.
+function AdjGet() -- Retrieves a random adjective from the adjective table, and removes it from the table.
+	local AdjNum = #AdjTable
+	local AdjX = math.random(AdjNum)
+	local AdjRet = AdjTable[AdjX][1]
+	table.remove(AdjTable, AdjX)
+	return AdjRet
+end
+
+local Adj1 = AdjGet()
+local Adj2 = AdjGet()
+local Adj3 = AdjGet()
+local Adj4 = AdjGet()
+local Adj5 = AdjGet()
+
+--[[ S02.2 - Noun Variables ]]--
+
+function NounGet() -- Retrieves a random noun from the noun table, and removes it from the table.
+	local NounNum = #NounTable
+	local NounX = math.random(NounNum)
+	local NounRet = NounTable[NounX][1]
+	table.remove(NounTable, NounX)
+	return NounRet
+end
+	
+local Noun1 = NounGet()
+local Noun2 = NounGet()
+local Noun3 = NounGet()
+local Noun4 = NounGet()
+local Noun5 = NounGet()
+
+--[[ S02.3- Verb Variables ]]--
+
+function VerbGet() -- Retrieves a random verb from the verb table, and removes it from the table.
+	local VerbNum = #VerbTable
+	local VerbX = math.random(VerbNum)
+	local VerbRet = VerbTable[VerbX][1]
+	table.remove(VerbTable, VerbX)
+	return VerbRet
+end
+
+local Verb1 = VerbGet()
+local Verb2 = VerbGet()
+local Verb3 = VerbGet()
+local Verb4 = VerbGet()
+local Verb5 = VerbGet()
+local Verb6 = VerbGet()
+local Verb7 = VerbGet()
+local Verb8 = VerbGet()
+
+--[[ S02.4- Name Variables ]]--
+
+function NameGet() -- Retrieves a random name from the name table, and removes it from the table.
+	local NameNum = #NameTable
+	local NameX = math.random(NameNum)
+	local NameRet = NameTable[NameX]
+	table.remove(NameTable, NameX)
+	return NameRet
+end
+
+local Name1 = NameGet()
+local Name2 = NameGet()
+local Name3 = NameGet()
+
+--[[ S02.5 - Sentence Variables INCOMPLETE ]]--
+
+local Sentence1 = SentFrags1[math.random(5)]
+local Sentence2 = SentFrags2[math.random(5)]
+local Sentence3 = SentFrags1[math.random(5)]
+local Sentence4 = SentFrags2[math.random(5)]
+local Sentence5 = SentFrags3[math.random(5)]
+
+
+--[[ S03 - SENTENCE OUTPUT ]]--
+
+RunState = true -- Sets the RunState to true, setting the state up for the following while loop.
+
+while RunState == true do
+
+print("\n" .. Sentence1 .. Noun1 .. Sentence2 .. Name1 .. ".") -- 
+
+RunState = false -- Sets the RunState to false, effectively ending this while loop.
+
+end
+
+while RunState == false do
+
+print("\nGoodbye!")
+return
+
+end
