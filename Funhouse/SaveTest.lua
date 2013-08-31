29aa94cfc5997a3d17d4f373ad8c1fd8548219fc
Alex <Trebek@users.noreply.github.com>
2013-08-30 19:14:43 -0700

    Create SaveTest.lua

---
+++ Funhouse/SaveTest.lua
@@ -0,0 +1,63 @@
+1
+false
+nil
+false
+nil
+false
+nil
+false
+false
+false
+true
+false
+false
+nil
+false
+nil
+false
+false
+false
+nil
+false
+nil
+false
+nil
+false
+nil
+false
+nil
+false
+nil
+1
+false
+nil
+false
+false
+true
+false
+nil
+false
+nil
+false
+nil
+false
+nil
+2
+false
+true
+false
+nil
+1
+false
+nil
+true
+true
+false
+true
+true
+{["N"]="Upstairs Hallway of your Home",["room"]="Your Bedroom",["S"]="Your Bathroom",}
+{["room"]="Your Bathroom",["N"]="Your Bedroom",}
+{["room"]="Upstairs Hallway of your Home",["S"]="Your Bedroom",["D"]="Living Room of your Home",}
+{["room"]="Living Room of your Home",["U"]="Upstairs Hallway of your Home",["S"]="Your Bedroom",}
+"Upstairs Hallway of your Home"
+24
